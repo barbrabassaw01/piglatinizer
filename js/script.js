c4ad0aee791a96889937212b772c5be452108816
barbrabassaw01 <bbassaw2022@bklawtech.com>
2019-11-06 23:25:03 +0000

    your message here

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
+$("#button").click(function(){
+    let userInput=$("input").val();
+$(".road").html(userInput);
+});
+$(".drill-1-button").click(function() {
+let name = $(".drill-1-input").val();
+    $(".drill-1-output").append("Welcome, " + name + "!");
+});
 
 
   
